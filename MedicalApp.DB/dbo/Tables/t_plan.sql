e4639cac5b28e52472ed11d565dce642f2d7195e
radu <rmitache@hotmail.com>
2018-02-16 01:04:45 +0100

    DB changes

---
+++ MedicalApp.DB/dbo/Tables/t_plan.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[t_plan] (
+    [id]           INT           IDENTITY (1, 1) NOT NULL,
+    [user_id]      INT           NOT NULL,
+    [name]         NVARCHAR (50) NULL,
+    [date_created] DATE          NOT NULL,
+    CONSTRAINT [PK_t_plan] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [FK_t_plan_t_user] FOREIGN KEY ([user_id]) REFERENCES [dbo].[t_user] ([id]) ON DELETE CASCADE
+);
+
---
+++ MedicalApp.DB/dbo/Tables/t_plan_version.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[t_plan_version] (
+    [id]         INT  IDENTITY (1, 1) NOT NULL,
+    [plan_id]    INT  NOT NULL,
+    [start_date] DATE NOT NULL,
+    [end_date]   DATE NULL,
+    CONSTRAINT [PK_t_plan_version] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [FK_t_plan_version_t_plan] FOREIGN KEY ([plan_id]) REFERENCES [dbo].[t_plan] ([id]) ON DELETE CASCADE
+);
+
