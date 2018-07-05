e6577cbb8a58c2834ca260d0f6c1f9cf3a182cb6
radu <rmitache@hotmail.com>
2018-07-05 18:37:45 +0200

    Missing files

---
+++ BLL.UnitTests/DomainModel/Plans/PlanServiceTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using BLL.DomainModel.Plans.BLOs;
+using BLL.DomainModel.Plans.Factories;
+using DataAccessLayer.Entities;
+using DataAccessLayer.Repositories.MedicineFactorRecordRepository;
+using DataAccessLayer.Repositories;
+using Moq;
+
+namespace BLL.UnitTests.DomainModel.Plans
+{
+    public class PlanServiceTests
+    {
+        // MethodName_Input_ExpectedOutput
+
+
+        [Fact]
+        public void UpdatePlan_PlanBLOWithMoreThanOneVersionAndLatestVersionHasIDZero_UpdatePreviousLastVersionAndAddTheNewLatestVersion()
+        {
+            // Mock dependencies
+            var planRepositoryMock = new Mock<IPlanRepository>();
+            var planFactoryMock = new Mock<IPlanFactory>();
+            var versionRepositoryMock = new Mock<IVersionRepository>();
+            var versionFactoryMock = new Mock<IVersionFactory>();
+
+        }
+
+        
+        //[Fact]
+        //public void ConvertToDataEntity_NormalDataEntity_MatchingFields()
+        //{
+        //    // Mock MedicineTypeFactory 
+        //    var medTypeFactoryMock = new Mock<IMedicineTypeFactory>();
+        //    medTypeFactoryMock
+        //        .Setup(factory => factory.Convert_ToBLO(
+        //            It.IsAny<TMedicineType>()))
+        //            .Returns(new Mock<MedicineType>().Object);
+
+        //    // Create the MedicineRuleItemFactory
+        //    var medicineRuleItemFactory = new MedicineRuleItemFactory(medTypeFactoryMock.Object);
+
+
+
+        //    // Test methods 
+
+
+        //    //// Mock the MedicineRuleItem BLO 
+        //    //var MedicineRuleItemBLOMock = new Mock<IMedicineTypeFactory>();
+
+        //    //var medRuleItemFactory
+        //}
+
+
+
+        //[Theory]
+        //[InlineData(" Mary", "  Jane  ", "Mary Jane")]
+        //[InlineData(" Bob", "Marley", "Bob Marley")]
+        //[InlineData(" Joe Hanson", " Lee   ", "Joe Hanson Lee")]
+        //public void GetFullName_ValidInputs_ReturnsCorrectResult_Example2(string firstName, string lastName, string expectedFullName)
+        //{
+        //    string fullNameResult = GetFullName(firstName, lastName);
+
+        //    Assert.Equal(expectedFullName, fullNameResult);
+        //}
+
+
+        //public static string GetFullName(string firstName, string lastName)
+        //{
+        //    if (string.IsNullOrWhiteSpace(firstName))
+        //    {
+        //        throw new ArgumentException("First name must be provided.");
+        //    }
+        //    if (string.IsNullOrWhiteSpace(lastName))
+        //    {
+        //        throw new ArgumentException("Last name must be provided.");
+        //    }
+
+        //    return $"{firstName.Trim()} {lastName.Trim()}".Trim();
+        //}
+    }
+}
---
+++ WebUI/SPA/Components/Pages/AnalysisPage/CommandFlows/change-selected-date-range.flow.ts
@@ -0,0 +1,39 @@
+﻿// Angular and 3rd party stuff
+import { Component, ChangeDetectorRef, ApplicationRef, ViewContainerRef, } from '@angular/core';
+import * as moment from 'moment';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+
+// Project modules
+import { Time, Range, TimeRange } from 'SPA/Core/Helpers/DataStructures/misc';
+import { CommandManager } from 'SPA/Core/Managers/CommandManager/command.manager';
+import { FlowDefinitions } from 'SPA/Components/Pages/HomePage/CommandFlows/flow-definitions';
+import * as CLOs from 'SPA/DomainModel/clo-exports';
+import { ModalDialogService } from 'SPA/Core/Services/ModalDialogService/modal-dialog.service';
+import { GenericCLOFactory } from 'SPA/DomainModel/generic-clo.factory';
+
+// Components
+import { AnalysisPageApplicationState } from 'SPA/Components/Pages/AnalysisPage/analysis-page-application-state';
+
+
+export function ChangeSelectedDateRangeFlow(
+    componentInstanceDictionary: { [componentClassName: string]: Object },
+    applicationStateRef: Object,
+    extraParams?: Object[]
+): Promise<any> {
+   
+    // Get strong typed parameters
+    const applicationState = applicationStateRef as AnalysisPageApplicationState;
+    const newDateRange = extraParams[0] as Range<moment.Moment>;
+
+
+    // Main logic --------------------------------------------------------------------------------------------------------------------------
+    let promise = new Promise<any>((resolve, reject) => {
+        applicationState.SelectedDateRange.Set(newDateRange);
+
+        resolve();
+    });
+    //--------------------------------------------------------------------------------------------------------------------------------------
+
+    return promise;
+}
