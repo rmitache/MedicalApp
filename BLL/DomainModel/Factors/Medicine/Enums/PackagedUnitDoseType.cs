4e3c7a542d4aaaeaccc0fc3da47daffbc7162676
radu <rmitache@hotmail.com>
2018-07-14 16:41:38 +0200

    Few entities modified

---
+++ BLL/DomainModel/Factors/Medicine/Enums/PackagedUnitDoseType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.DomainModel.Factors.Medicine.Enums
+{
+    public enum PackagedUnitDoseType
+    {
+        Pills = 0,
+        Satchels = 1,
+        DispenserDoses = 2,
+    }
+
+}
---
+++ BLL/DomainModel/Factors/Medicine/Enums/UserDefinedUnitDoseType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BLL.DomainModel.Factors.Medicine.Enums
+{
+
+
+    public enum UserDefinedUnitDoseType
+    {
+        Teaspoons = 0,
+        Drops = 1,
+        Custom = 2
+
+        //Teaspoons = 3,
+        //Drops = 4,
+        //Custom = 5
+    }
+}
