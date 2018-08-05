b0161bbf1695c7800271ad68ea99ee0f17d8249d
radu <rmitache@hotmail.com>
2018-08-05 09:03:44 +0200

    Changes

---
+++ BLL.UnitTests/Common/TimeTests.cs
@@ -0,0 +1,12 @@
+﻿using Common.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BLL.UnitTests.Common
+{
+    public class TimeTests
+    {
+    }
+}
