cce9d4ee3c74e14215ee4e95008a6b719b748fe0
radu <rmitache@hotmail.com>
2018-08-30 14:18:44 +0200

    More changes

--- WebUI/SPA/Components/Pages/HomePage/PlansOverview/PlanElem/plan-elem.component.ts
+++ WebUI/SPA/Components/Pages/HomePage/PlansOverview/PlanElem/plan-elem.component.ts
@@ -1,7 +1,6 @@
 // Angular and 3rd party stuff
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import * as moment from 'moment';
-import { MenuItem } from 'primeng/primeng';
 
 // Project modules
 import * as CLOs from 'SPA/DomainModel/clo-exports';
@@ -10,178 +9,212 @@ import { StringToColour } from 'SPA/Core/Helpers/Functions/functions';
 
 // Components
 import { PlanActionType } from 'SPA/Components/Pages/HomePage/PlansOverview/plans-overview.component';
+import { MenuItem } from 'SPA/Components/Shared/SplitButton/split-button.component';
 
 @Component({
-    selector: 'plan-elem',
-    templateUrl: './plan-elem.component.html',
-    styleUrls: ['./plan-elem.component.css'],
-    host: { 'class': 'plan-elem' }
+	selector: 'plan-elem',
+	templateUrl: './plan-elem.component.html',
+	styleUrls: ['./plan-elem.component.css'],
+	host: { 'class': 'plan-elem' }
 })
 export class PlanElemComponent {
-    // Fields
-    @Input('PlanCLO')
-    private readonly planCLO: CLOs.PlanCLO;
-    private readonly planStatusesEnum = Enums.PlanStatus;
-    private menuItemsToPlanStatusMap: MenuItemsToPLanStatusMap = {
-        Active: [
-            {
-                label: 'Adjust',
-                command: (event) => {
-                    this.ActionTriggered.emit([this.planCLO, PlanActionType.Adjust]);
+	// Fields
+	@Input('PlanCLO')
+	private readonly planCLO: CLOs.PlanCLO;
+	private readonly planStatusesEnum = Enums.PlanStatus;
+	private menuItemsToPlanStatusMap: MenuItemsToPLanStatusMap = {
+		Active: [
+			//{
+			//	Name: 'Adjust',
+			//	OnClick: () => {
+			//		this.ActionTriggered.emit([this.planCLO, PlanActionType.Adjust]);
 
-                }
-            },
-            {
-                label: 'Hard edit latest version',
-                command: (event) => {
-                    this.ActionTriggered.emit([this.planCLO, PlanActionType.HardEdit]);
+			//	}
+			//},
+			{
+				Name: 'Hard edit latest version',
+				OnClick: () => {
+					this.ActionTriggered.emit([this.planCLO, PlanActionType.HardEdit]);
 
-                }
-            }
-        ],
-        ActiveWithUpcomingAdjustment: [
-            {
-                label: 'Hard edit',
-                command: (event) => {
-                    this.ActionTriggered.emit([this.planCLO, PlanActionType.HardEdit]);
-                }
-            }
-        ],
-        Inactive: [
-            {
-                label: 'Re-start',
-                command: (event) => {
-                    this.ActionTriggered.emit([this.planCLO, PlanActionType.Restart]);
-                }
-            }
-        ],
-        UpcomingAsNew: [
-            {
-                label: 'Hard modify',
-                command: (event) => {
-                    this.ActionTriggered.emit([this.planCLO, PlanActionType.HardEdit]);
-                }
-            }
-        ],
-        UpcomingAsRestarted: [
-            {
-                label: 'Hard modify',
-                command: (event) => {
-                    this.ActionTriggered.emit([this.planCLO, PlanActionType.HardEdit]);
-                }
-            }
-        ]
-    };
-    private readonly viewModel: ViewModel = {
-        PlanCLO: null,
-        Color: null,
-        GetMenuItems: () => {
-            let planStatusName = Enums.PlanStatus[this.viewModel.PlanCLO.Status];
-            return this.menuItemsToPlanStatusMap[planStatusName];
-        },
+				}
+			}
+		],
+		ActiveWithUpcomingAdjustment: [
+			{
+				Name: 'Hard edit',
+				OnClick: () => {
+					this.ActionTriggered.emit([this.planCLO, PlanActionType.HardEdit]);
+				}
+			}
+		],
+		Inactive: [
+			{
+				Name: 'Re-start',
+				OnClick: () => {
+					this.ActionTriggered.emit([this.planCLO, PlanActionType.Restart]);
+				}
+			}
+		],
+		UpcomingAsNew: [
+			{
+				Name: 'Hard modify',
+				OnClick: () => {
+					this.ActionTriggered.emit([this.planCLO, PlanActionType.HardEdit]);
+				}
+			}
+		],
+		UpcomingAsRestarted: [
+			{
+				Name: 'Hard modify',
+				OnClick: () => {
+					this.ActionTriggered.emit([this.planCLO, PlanActionType.HardEdit]);
+				}
+			}
+		]
+	};
+	private mainActionToPlanStatusMap: MainActionsToPLanStatusMap = {
+		Active: {
+			Icon: 'fa fa-pencil',
+			ButtonClass: '',
+			OnClick: () => {
+				this.ActionTriggered.emit([this.planCLO, PlanActionType.Adjust]);
 
-        StatusString: null,
-        StartDatePrefixString: null,
-        RelativeStartDateString: null,
-        EndDatePrefixString: null,
-        RelativeEndDateString: null
-    };
+			}
+		}
+	};
+	private readonly viewModel: ViewModel = {
+		PlanCLO: null,
+		Color: null,
+		GetMenuItems: () => {
+			let planStatusName = Enums.PlanStatus[this.viewModel.PlanCLO.Status];
+			return this.menuItemsToPlanStatusMap[planStatusName];
+		},
+		MainAction: null,
 
-    // Private methods
-    private getRelativeDateAsString(refDate: Date) {
-        return moment(refDate).calendar(null, {
-            lastDay: '[Yesterday]',
-            sameDay: '[Today]',
-            nextDay: '[Tomorrow]',
-            lastWeek: 'MMM DD, YYYY',//'[Last] dddd',
-            nextWeek: 'MMM DD, YYYY',//'[Next] dddd',
-            sameElse: 'MMM DD, YYYY',
-            //sameElse: (dt) => {
-            //    return 'wtf';
-            //}
-        });
+		StatusString: null,
+		StartDatePrefixString: null,
+		RelativeStartDateString: null,
+		EndDatePrefixString: null,
+		RelativeEndDateString: null
+	};
 
-
-    }
-
-
-    // Constructor 
-    constructor(
-    ) {
-    }
-    ngOnInit() {
-        this.viewModel.PlanCLO = this.planCLO;
-        this.viewModel.StatusString = Enums.PlanStatus[this.planCLO.Status];
-        this.viewModel.Color = StringToColour(this.viewModel.PlanCLO.Name);
-
-        // StartDate and EndDate labels according to Plan.Status
-        let latestVersion = this.planCLO.GetLatestVersion();
-        let firstVersion = this.planCLO.GetFirstVersion();
-        switch (this.planCLO.Status) {
-            // Active
-            case Enums.PlanStatus.Active:
-                if (this.planCLO.Versions.Length > 1) {
-                    this.viewModel.StartDatePrefixString = 'Adjusted/restarted:';
-                    this.viewModel.RelativeStartDateString = this.getRelativeDateAsString(latestVersion.StartDateTime);
-                } else {
-                    this.viewModel.StartDatePrefixString = 'Started:';
-                    this.viewModel.RelativeStartDateString = this.getRelativeDateAsString(firstVersion.StartDateTime);
-                }
-
-                let endDate = moment(latestVersion.EndDateTime).startOf('day');
-                if (this.planCLO.GetLatestVersion().EndDateTime !== null) {
-                    this.viewModel.EndDatePrefixString = 'Ending:';
-                    this.viewModel.RelativeEndDateString = this.getRelativeDateAsString(latestVersion.EndDateTime);
-                }
-                break;
-
-            // ActiveWithUpcomingAdjustment
-            case Enums.PlanStatus.ActiveWithUpcomingAdjustment:
-                this.viewModel.StartDatePrefixString = 'Will change:';
-                this.viewModel.RelativeStartDateString = this.getRelativeDateAsString(latestVersion.StartDateTime);
-                if (this.planCLO.GetLatestVersion().EndDateTime !== null) {
-                    this.viewModel.EndDatePrefixString = 'Will end:';
-                    this.viewModel.RelativeEndDateString = moment(latestVersion.EndDateTime).format('MMM DD, YYYY');
-                }
-                break;
-
-            // Inactive
-            case Enums.PlanStatus.Inactive:
-                this.viewModel.EndDatePrefixString = 'Ended:';
-                this.viewModel.RelativeEndDateString = this.getRelativeDateAsString(latestVersion.EndDateTime);
-                break;
-
-            // UpcomingAsNew
-            case Enums.PlanStatus.UpcomingAsNew:
-                this.viewModel.StartDatePrefixString = 'Will start:';
-                this.viewModel.RelativeStartDateString = this.getRelativeDateAsString(latestVersion.StartDateTime);
-                break;
-
-            // UpcomingAsRestarted
-            case Enums.PlanStatus.UpcomingAsRestarted:
-                this.viewModel.StartDatePrefixString = 'Will restart:';
-                this.viewModel.RelativeStartDateString = this.getRelativeDateAsString(latestVersion.StartDateTime);
-                break;
-        }
+	// Private methods
+	private getRelativeDateAsString(refDate: Date) {
+		return moment(refDate).calendar(null, {
+			lastDay: '[Yesterday]',
+			sameDay: '[Today]',
+			nextDay: '[Tomorrow]',
+			lastWeek: 'MMM DD, YYYY',//'[Last] dddd',
+			nextWeek: 'MMM DD, YYYY',//'[Next] dddd',
+			sameElse: 'MMM DD, YYYY',
+			//sameElse: (dt) => {
+			//    return 'wtf';
+			//}
+		});
 
 
-    }
+	}
 
-    // Events
-    @Output() public ActionTriggered: EventEmitter<any> = new EventEmitter();
+
+	// Constructor 
+	constructor(
+	) {
+	}
+	ngOnInit() {
+		this.viewModel.PlanCLO = this.planCLO;
+		this.viewModel.StatusString = Enums.PlanStatus[this.planCLO.Status];
+		this.viewModel.Color = StringToColour(this.viewModel.PlanCLO.Name);
+
+		// StartDate and EndDate labels according to Plan.Status
+		let latestVersion = this.planCLO.GetLatestVersion();
+		let firstVersion = this.planCLO.GetFirstVersion();
+		switch (this.planCLO.Status) {
+			// Active
+			case Enums.PlanStatus.Active:
+				if (this.planCLO.Versions.Length > 1) {
+					this.viewModel.StartDatePrefixString = 'Adjusted/restarted:';
+					this.viewModel.RelativeStartDateString = this.getRelativeDateAsString(latestVersion.StartDateTime);
+				} else {
+					this.viewModel.StartDatePrefixString = 'Started:';
+					this.viewModel.RelativeStartDateString = this.getRelativeDateAsString(firstVersion.StartDateTime);
+				}
+
+				let endDate = moment(latestVersion.EndDateTime).startOf('day');
+				if (this.planCLO.GetLatestVersion().EndDateTime !== null) {
+					this.viewModel.EndDatePrefixString = 'Ending:';
+					this.viewModel.RelativeEndDateString = this.getRelativeDateAsString(latestVersion.EndDateTime);
+				}
+				break;
+
+			// ActiveWithUpcomingAdjustment
+			case Enums.PlanStatus.ActiveWithUpcomingAdjustment:
+				this.viewModel.StartDatePrefixString = 'Will change:';
+				this.viewModel.RelativeStartDateString = this.getRelativeDateAsString(latestVersion.StartDateTime);
+				if (this.planCLO.GetLatestVersion().EndDateTime !== null) {
+					this.viewModel.EndDatePrefixString = 'Will end:';
+					this.viewModel.RelativeEndDateString = moment(latestVersion.EndDateTime).format('MMM DD, YYYY');
+				}
+				break;
+
+			// Inactive
+			case Enums.PlanStatus.Inactive:
+				this.viewModel.EndDatePrefixString = 'Ended:';
+				this.viewModel.RelativeEndDateString = this.getRelativeDateAsString(latestVersion.EndDateTime);
+				break;
+
+			// UpcomingAsNew
+			case Enums.PlanStatus.UpcomingAsNew:
+				this.viewModel.StartDatePrefixString = 'Will start:';
+				this.viewModel.RelativeStartDateString = this.getRelativeDateAsString(latestVersion.StartDateTime);
+				break;
+
+			// UpcomingAsRestarted
+			case Enums.PlanStatus.UpcomingAsRestarted:
+				this.viewModel.StartDatePrefixString = 'Will restart:';
+				this.viewModel.RelativeStartDateString = this.getRelativeDateAsString(latestVersion.StartDateTime);
+				break;
+		}
+
+		let planStatusName = Enums.PlanStatus[this.viewModel.PlanCLO.Status];
+		this.viewModel.MainAction = this.mainActionToPlanStatusMap[planStatusName];
+
+	}
+
+	// Event handlers
+	private onAdjustTriggered() {
+		this.ActionTriggered.emit([this.planCLO, PlanActionType.Adjust]);
+
+	}
+
+	// Events
+	@Output() public ActionTriggered: EventEmitter<any> = new EventEmitter();
 }
 interface ViewModel {
-    PlanCLO: CLOs.PlanCLO;
+	PlanCLO: CLOs.PlanCLO;
 
-    GetMenuItems(): MenuItem[];
-    Color: string;
-    StatusString: string;
-    StartDatePrefixString: string;
-    RelativeStartDateString: string;
-    EndDatePrefixString: string;
-    RelativeEndDateString: string;
+	GetMenuItems();
+	MainAction: MainActionButton;
+
+	Color: string;
+	StatusString: string;
+	StartDatePrefixString: string;
+	RelativeStartDateString: string;
+	EndDatePrefixString: string;
+	RelativeEndDateString: string;
+
+
 }
 interface MenuItemsToPLanStatusMap {
-    [planStatusName: string]: MenuItem[];
+	[planStatusName: string]: MenuItem[];
 
+}
+
+interface MainActionsToPLanStatusMap {
+	[planStatusName: string]: MainActionButton;
+
+}
+interface MainActionButton {
+	Icon: string;
+	ButtonClass: string;
+	OnClick();
 }
--- WebUI/SPA/Components/Shared/SplitButton/split-button.component.ts
+++ WebUI/SPA/Components/Shared/SplitButton/split-button.component.ts
@@ -4,79 +4,82 @@ import * as $ from 'jquery';
 
 
 @Component({
-    selector: 'split-button',
-    templateUrl: './split-button.component.html',
-    styleUrls: ['./split-button.component.css'],
-    host: { 'class': 'split-button' }
+	selector: 'split-button',
+	templateUrl: './split-button.component.html',
+	styleUrls: ['./split-button.component.css'],
+	host: { 'class': 'split-button' }
 })
 export class SplitButtonComponent {
-    // Fields
-    @Input('MenuItems')
+	// Fields
+	@Input('MenuItems')
 	private menuItems: MenuItem[];
 	@ViewChild('menuItemsBar') menuItemsBarElement: ElementRef;
 	@ViewChild('button') buttonElement: ElementRef;
-    private readonly viewModel: ViewModel = {
-        MenuItems: null,
-        MenuPanelVisible: false,
+	private readonly viewModel: ViewModel = {
+		MenuItems: null,
+		MenuPanelVisible: false,
 
-        MenuPanelLeftPosition: null,
-        MenuPanelTopPosition: null
-    };
-    private globalListenFunction: Function;
+		MenuPanelLeftPosition: null,
+		MenuPanelTopPosition: null
+	};
+	private globalListenFunction: Function;
 
-    // Constructor 
-    constructor(
-        private eRef: ElementRef,
-        private renderer: Renderer) {
+	// Constructor 
+	constructor(
+		private eRef: ElementRef,
+		private renderer: Renderer) {
 
-    }
-    ngOnInit() {
-        this.viewModel.MenuItems = this.menuItems;
-    }
+	}
+	ngOnInit() {
+		this.viewModel.MenuItems = this.menuItems;
+	}
 	buttonRight: number;
 
-    // Events
-    @Output() public ItemClicked: EventEmitter<MenuItem> = new EventEmitter();
+	// Events
+	@Output() public ItemClicked: EventEmitter<MenuItem> = new EventEmitter();
 
-    // Event handlers
-    private onSplitButtonClicked($event: Event) {
-        // Hide menu panel 
-        if (this.viewModel.MenuPanelVisible === true) {
-            this.viewModel.MenuPanelVisible = false;
-            this.viewModel.MenuPanelLeftPosition = null;
-        }
-        // Position and show menu panel
-        else {
-            this.viewModel.MenuPanelVisible = true;
+	// Event handlers
+	private onSplitButtonClicked($event: Event) {
+		// Hide menu panel 
+		if (this.viewModel.MenuPanelVisible === true) {
+			this.viewModel.MenuPanelVisible = false;
+			this.viewModel.MenuPanelLeftPosition = null;
+		}
+		// Position and show menu panel
+		else {
+			this.viewModel.MenuPanelVisible = true;
+
 			var panelWidth = (this.menuItemsBarElement.nativeElement as HTMLElement).clientWidth;
 			var buttonWidth = (this.buttonElement.nativeElement as HTMLElement).clientWidth;
-
 			let buttonLeftOffset = (this.buttonElement.nativeElement as HTMLElement).offsetLeft;
-			this.viewModel.MenuPanelLeftPosition = -panelWidth + buttonWidth + buttonLeftOffset;
-            document.addEventListener('click', this.onClickedOutside, true); // Add global click handler to detect "click outside" actions
-        }
-    }
-    private onClickedOutside = ($event: Event) => {
-        this.viewModel.MenuPanelVisible = false;
 
-        // Remove handler 
-        document.removeEventListener('click', this.onClickedOutside, true);
-    }
-    private onMenuItemClicked = (menuItem: MenuItem) => {
-        menuItem.OnClick();
-    }
+			this.viewModel.MenuPanelLeftPosition = -panelWidth + buttonWidth + buttonLeftOffset;
+			document.addEventListener('click', this.onClickedOutside, true); // Add global click handler to detect "click outside" actions
+
+		}
+	}
+	private onClickedOutside = ($event: Event) => {
+		this.viewModel.MenuPanelVisible = false;
+
+		// Remove handler 
+		document.removeEventListener('click', this.onClickedOutside, true);
+	}
+	private onMenuItemClicked = (menuItem: MenuItem) => {
+		menuItem.OnClick();
+	}
 
 
 }
 
 
 interface ViewModel {
-    MenuItems: MenuItem[];
-    MenuPanelVisible: boolean;
-    MenuPanelLeftPosition: number;
-    MenuPanelTopPosition: number;
+	MenuItems: MenuItem[];
+	MenuPanelVisible: boolean;
+	MenuPanelLeftPosition: number;
+	MenuPanelTopPosition: number;
 }
 export interface MenuItem {
-    Name: string;
-    OnClick();
+	Name: string;
+	OnClick();
+	Icon?: string;
 }
