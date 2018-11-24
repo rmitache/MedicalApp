2e74348fc1834e59d92222cc321f36cee67f199b
Radu Mitache <rmitache@hotmail.com>
2018-11-24 19:35:21 +0100

    Fixed issue with HealthGraph and primeng chart

--- WebUI/SPA/Components/Pages/HomePage/HealthGraph/health-graph.component.ts
+++ WebUI/SPA/Components/Pages/HomePage/HealthGraph/health-graph.component.ts
@@ -3,7 +3,8 @@ import { Component, Input, ViewContainerRef, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { Subscription } from 'rxjs/Subscription';
 import * as moment from 'moment';
-import { ChartModule, UIChart } from 'primeng/primeng';
+import * as Chart from 'chart.js';
+
 
 // Project modules
 import * as CLOs from 'SPA/DomainModel/clo-exports';
@@ -33,13 +34,14 @@ import { AddHealthStatusDialogService } from '../../../Shared/Popups/AddHealthSt
 export class HealthGraphComponent {
     // Fields
     private availableWindowPaddingInMonths = 0;
-    @ViewChild('chart')
-    private chartInstance: UIChart;
     @ViewChild('healthStatusTooltip')
     private healthStatusTooltipInstance: SingleHealthStatusTooltipComponent;
     @ViewChild('navPanel')
     private navPanelInstance: NavigationPanelComponent;
     private readonly noDataModes = NoDataModes;
+    private canvas: any;
+    private chartCanvasContext: any;
+    private chartInstance: any;
     private readonly viewModel: ViewModel = {
         AvailableDateRange: null,
         AvailableHealthEntries: null,
@@ -58,10 +60,17 @@ export class HealthGraphComponent {
 
     // Private methods
     private getCurrentDisplayModeInstance(): IDisplayMode {
+        var getChartCanvasFunction: GetChartCanvasFunc = () => {
+            return this.canvas;
+        };
+        var getChartInstanceFunction: GetChartInstanceFunc = () => {
+            return this.chartInstance;
+        };
+
         // Get Current Mode strategy
         let currentStrategy: IDisplayMode = null;
         if (this.viewModel.DateRangeDisplayMode === DateRangeMode.SingleDay) {
-            currentStrategy = new DayDisplayMode(this.chartInstance, this.healthStatusTooltipInstance);
+            currentStrategy = new DayDisplayMode(getChartInstanceFunction, getChartCanvasFunction, this.healthStatusTooltipInstance);
         } else {
             // OBS -> Not implemented yet
             throw new Error('HealthGraphDisplayMode not implemented yet');
@@ -100,7 +109,18 @@ export class HealthGraphComponent {
         this.viewModel.ChartOptions = currentDisplayMode.GenerateChartOptions(datesToCLOsDictionary);
         this.viewModel.ChartData = currentDisplayMode.GenerateChartData(datesToCLOsDictionary,
             new Range<moment.Moment>(this.viewModel.SelectedDateRange.RangeStart.clone(), this.viewModel.SelectedDateRange.RangeEnd.clone()));
-        this.chartInstance.reinit();
+
+
+        // Recreate the chart
+        if (this.chartInstance) {
+            this.chartInstance.destroy();
+        }
+        this.chartInstance = new Chart(this.chartCanvasContext, {
+            type: 'line',
+            data: this.viewModel.ChartData,
+            options: this.viewModel.ChartOptions,
+
+        });
 
         // NoData triggers
         if (filteredHealthStatusEntryCLOs.length === 0) {
@@ -140,6 +160,13 @@ export class HealthGraphComponent {
         this.viewModel.UserHasAnyHealthStatusEntries = this.dataService.GetHealthStatusEntriesForInitialRangeFromBundle().UserHasAnyHealthStatusEntries;
         this.viewModel.SelectedDateRange = initialSelectedDateRange;
 
+
+    }
+    ngAfterViewInit() {
+        // Get references to the chart canvas context
+        this.canvas = document.getElementById('myChart');
+        this.chartCanvasContext = this.canvas.getContext('2d');
+
         // Refresh the UI
         this.refreshUI();
     }
@@ -149,10 +176,7 @@ export class HealthGraphComponent {
 
     // Event handlers
     private onAddNewHealthStatusEntryTriggered() {
-
         this.addHealthStatusDialogService.Open(this.viewContainerRef, () => {
-
-
             this.reloadDataFromServer(this.viewModel.AvailableDateRange)
                 .then(() => {
                     this.refreshUI();
@@ -161,7 +185,6 @@ export class HealthGraphComponent {
                     }, 200);
                 });
         });
-
     }
     private onSelectedDateRangeChangedBackward(newSelDateRange: Range<moment.Moment>) {
         // Check if newSelDateRange is within the AvailableDateRange        
@@ -329,7 +352,8 @@ class DayDisplayMode implements IDisplayMode {
 
     // Constructor
     constructor(
-        private readonly chartInstance: UIChart,
+        private readonly getChartInstance: GetChartInstanceFunc,
+        private readonly getChartCanvas: GetChartCanvasFunc,
         private readonly healthStatusTooltipInstance: SingleHealthStatusTooltipComponent) {
     }
 
@@ -352,10 +376,10 @@ class DayDisplayMode implements IDisplayMode {
                     let index = tooltipModel.dataPoints[0].index;
                     let healthLevel = tooltipModel.dataPoints[0].yLabel;
 
-                    let dataPointColor = this.chartInstance.data.datasets[0].pointBackgroundColor[index];
+                    let dataPointColor = this.getChartInstance().data.datasets[0].pointBackgroundColor[index];
                     let targetHealthStatusCLO = this.getHealthStatusCLOByDateTimeAndHealthLevel(moment(dateTimeString),
                         healthLevel, datesToCLOsDictionary);
-                    let parentPosition = (this.chartInstance.el.nativeElement as HTMLElement).getBoundingClientRect();
+                    let parentPosition = this.getChartCanvas().getBoundingClientRect();
 
                     //
                     this.healthStatusTooltipInstance.SetDataAndPosition(targetHealthStatusCLO, parentPosition,
@@ -487,218 +511,5 @@ class DayDisplayMode implements IDisplayMode {
 };
 
 
-/*
-class MonthDisplayMode implements IDisplayMode {
-    // Private methods
-    private getAverageHealthLevel(healthStatusEntryCLOs: CLOs.HealthStatusEntryCLO[]) {
-        var sum: number = 0;
-        var result = 0;
-        healthStatusEntryCLOs.forEach(clo => {
-            sum += clo.HealthLevel;
-        });
-
-        if (healthStatusEntryCLOs.length > 0)
-            result = sum / healthStatusEntryCLOs.length;
-        else
-            result = null;
-
-
-        if (result === 0) {
-            result = 0.2;
-        }
-        return result;
-    }
-    private generateDataPointsForChart(datesToCLOsDictionary: { [dateKey: string]: CLOs.HealthStatusEntryCLO[] }, range: Range<moment.Moment>) {
-
-        // Variables
-        var dataPoints = []
-        var dataPointsBgColors = [];
-
-
-        // Loop through dates and create datapoints
-        var datesInRangeArray = HelperFunctions.EnumerateDaysBetweenDatesUsingMoment(range, true);
-        datesInRangeArray.forEach((date, index) => {
-
-            // Prepare data
-            let dateKey = date.format('DD/MM/YYYY');
-            var clos = (datesToCLOsDictionary[dateKey] !== undefined) ? datesToCLOsDictionary[dateKey] : [];
-            var avgHealthLevel = this.getAverageHealthLevel(clos);
-
-            // Test comment
-            // Create datapoint
-            var dp = {
-                x: date,
-                y: avgHealthLevel
-            };
-            dataPoints.push(dp);
-
-            // Basic colors
-            if (avgHealthLevel >= 0) {
-            	dataPointsBgColors.push('#9dc340');
-            } else if (avgHealthLevel < 0) {
-            	dataPointsBgColors.push('#fe6060');
-            } 
-
-
-            //// great
-            //if (avgHealthLevel >= 2) {
-            //    dataPointsBgColors.push('green');
-            //}
-            //// good
-            //else if (avgHealthLevel >= 1 && avgHealthLevel < 2) {
-            //    dataPointsBgColors.push('#9dc340');
-            //}
-            //// ok
-            //else if (avgHealthLevel >= 0 && avgHealthLevel < 1) {
-            //    dataPointsBgColors.push('#d8e27e');
-            //}
-            //// notgreat 
-            //else if (avgHealthLevel > -0.8 && avgHealthLevel < 0) {
-            //    dataPointsBgColors.push('#f3a590');
-            //}
-            //// bad 
-            //else if (avgHealthLevel >= -2 && avgHealthLevel <= -0.8) {
-            //    dataPointsBgColors.push('#fe6060');
-            //}
-            //// very bad
-            //else if (avgHealthLevel < -1) {
-            //    dataPointsBgColors.push('red');
-            //}
-
-        });
-
-        return {
-            dataPoints: dataPoints,
-            dataPointsBgColors: dataPointsBgColors
-        };
-    }
-
-    // Constructor
-    constructor(
-        private readonly chartInstance: UIChart,
-        private readonly graphTooltipInstance: GraphTooltipComponent) {
-    }
-
-
-    // Public methods
-    public GenerateChartOptions(datesToCLOsDictionary: { [dateKey: string]: CLOs.HealthStatusEntryCLO[] }) {
-        let chartOptions = {
-            tooltips: {
-                enabled: false,
-                custom: (tooltipModel) => {
-                    if (tooltipModel.opacity === 0) {
-                        this.graphTooltipInstance.HideAndClear();
-                        return;
-                    }
-                    
-                    // Variables
-                    var dateString = tooltipModel.title[0];
-                    var avgHealthLevelValue = tooltipModel.dataPoints[0].yLabel;
-                    var dateKey = moment(dateString, "dddd MMM D, YYYY").format('DD/MM/YYYY');
-                    var healthStatusEntryCLOsForDate = datesToCLOsDictionary[dateKey];
-                    var parentPosition = (this.chartInstance.el.nativeElement as HTMLElement).getBoundingClientRect();
-
-                    // Hack for positioning of tooltip on negative bar chart values
-                    var value = tooltipModel.dataPoints[0].yLabel;
-                    if (value < 0) {
-                        tooltipModel.caretY = (parentPosition.height / 2);
-                    }
-
-                    //
-                    this.graphTooltipInstance.SetDataAndPosition(dateString, healthStatusEntryCLOsForDate, parentPosition,
-                        tooltipModel.caretX, tooltipModel.caretY, avgHealthLevelValue);
-                }
-            },
-            elements: {
-                line: {
-                    tension: 0, // disables bezier curves
-                }
-            },
-            legend: {
-                display: false,
-                position: 'top',
-                labels: {
-                    boxWidth: 15,
-
-                },
-            },
-            scales: {
-                xAxes: [{
-                    type: "time",
-                    time: {
-                        unit: 'day',
-                        round: 'day',
-                        unitStepSize: 1,
-                        tooltipFormat: "dddd MMM D, YYYY",
-                        displayFormats: {
-                            hour: 'MMM D'
-                        }
-                    },
-                    gridLines: {
-                        color: '#cacaca',
-                        display: true,
-                        drawOnChartArea: false,
-                    },
-                    ticks: {
-                        maxRotation: 0,
-                        minRotation: 0,
-                        fontColor: 'gray',
-                        fontFamily: 'Arial',
-                        fontSize: 10,
-                        beginAtZero: true,
-                        autoSkip: false,
-                        callback: function (value, index, values) {
-                            if (!(index % 3)) return value;
-                        }
-                    }
-                }],
-                yAxes: [{
-                    gridLines: {
-                        display: true,
-                        drawTicks: true,
-                        drawOnChartArea: true,
-                        tickMarkLength: 5,
-                        drawBorder: true,
-                        zeroLineColor: 'gray'
-                    },
-
-                    ticks: {
-                        fontColor: 'gray',
-                        beginAtZero: true,
-                        min: -3,
-                        max: 3,
-                        stepSize: 1,
-                        callback: function (label, index, labels) {
-                            //if (label !== 0)
-                            //    return Enums.HealthLevel[label];
-                            //else
-                                return '';
-
-                        }
-                    }
-                }]
-            },
-            responsive: true,
-            maintainAspectRatio: false
-        };
-        return chartOptions;
-    }
-    public GenerateChartData(datesToCLOsDictionary: { [dateKey: string]: CLOs.HealthStatusEntryCLO[] }, currentSelDateRange: Range<moment.Moment>) {
-
-        // Prepare data
-        var dataPointsInfo = this.generateDataPointsForChart(datesToCLOsDictionary, currentSelDateRange);
-
-        // Set data
-        var data = {
-            datasets: [
-                {
-                    data: dataPointsInfo.dataPoints,
-                    backgroundColor: dataPointsInfo.dataPointsBgColors,
-                }
-            ]
-        }
-        return data;
-    }
-
-};
-*/
+type GetChartCanvasFunc = () => any;
+type GetChartInstanceFunc = () => any;
