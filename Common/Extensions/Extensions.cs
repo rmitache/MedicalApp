ea36d3589b8281357e9e2016210d9b9e1ca69ea8
Radu Mitache <rmitache@hotmail.com>
2018-12-04 06:25:51 +0100

    Fix

--- Common/Extensions/Extensions.cs
+++ Common/Extensions/Extensions.cs
@@ -13,7 +13,7 @@ namespace Common
             {
                 case null: throw new ArgumentNullException(nameof(input));
                 case "": throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input));
-                default: return input.First().ToString().ToUpper() + input.Substring(1);
+                default: return input.First().ToString().ToUpper() + input.Substring(1).ToLower();
             }
         }
 
