048cae75a9a12d8fadcada7aed31b39fc4ad2b01
Radu Mitache <rmitache@hotmail.com>
2019-02-20 20:11:10 +0100

    DB changes

--- MedicalApp.DB/dbo/Tables/t_medicine_type.sql
+++ MedicalApp.DB/dbo/Tables/t_medicine_type.sql
@@ -1,15 +1,17 @@
 ﻿CREATE TABLE [dbo].[t_medicine_type] (
-    [id]                         INT           IDENTITY (1, 1) NOT NULL,
-    [user_id]                    INT           NOT NULL,
-    [name]                       NVARCHAR (50) NOT NULL,
-    [producer_name]              NVARCHAR (50) NULL,
-    [base_unit_of_measure_id]    INT           NOT NULL,
-    [is_packaged_into_units]     BIT           NOT NULL,
-    [packaged_unit_dose_type_id] INT           NULL,
-    [packaged_unit_dose_size]    INT           NULL,
+    [id]                         INT             IDENTITY (1, 1) NOT NULL,
+    [user_id]                    INT             NOT NULL,
+    [name]                       NVARCHAR (50)   NOT NULL,
+    [producer_name]              NVARCHAR (50)   NULL,
+    [base_unit_of_measure_id]    INT             NOT NULL,
+    [is_packaged_into_units]     BIT             NOT NULL,
+    [packaged_unit_dose_type_id] INT             NULL,
+    [packaged_unit_dose_size]    DECIMAL (18, 4) NULL,
     CONSTRAINT [PK_t_medicine_type] PRIMARY KEY CLUSTERED ([id] ASC),
     CONSTRAINT [FK_t_medicine_type_t_user] FOREIGN KEY ([user_id]) REFERENCES [dbo].[t_user] ([id])
 );
+
+
 
 
 
--- MedicalApp.DB/dbo/Tables/t_plan_medicine_rule_item.sql
+++ MedicalApp.DB/dbo/Tables/t_plan_medicine_rule_item.sql
@@ -1,15 +1,15 @@
 ﻿CREATE TABLE [dbo].[t_plan_medicine_rule_item] (
-    [id]                             INT IDENTITY (1, 1) NOT NULL,
-    [plan_rule_id]                   INT NOT NULL,
-    [medicine_type_id]               INT NOT NULL,
-    [unit_dose_quantifier]           INT NOT NULL,
-    [has_user_defined_unit_dose]     BIT NOT NULL,
-    [user_defined_unit_dose_type_id] INT NULL,
-    [user_defined_unit_dose_size]    INT NULL,
-    CONSTRAINT [PK_t_plan_medicine_rule_item] PRIMARY KEY CLUSTERED ([id] ASC),
-    CONSTRAINT [FK_t_plan_medicine_rule_item_t_medicine_type] FOREIGN KEY ([medicine_type_id]) REFERENCES [dbo].[t_medicine_type] ([id]),
-    CONSTRAINT [FK_t_plan_medicine_rule_item_t_plan_rule] FOREIGN KEY ([plan_rule_id]) REFERENCES [dbo].[t_plan_rule] ([id]) ON DELETE CASCADE
+    [id]                             INT             IDENTITY (1, 1) NOT NULL,
+    [plan_rule_id]                   INT             NOT NULL,
+    [medicine_type_id]               INT             NOT NULL,
+    [unit_dose_quantifier]           INT             NOT NULL,
+    [has_user_defined_unit_dose]     BIT             NULL,
+    [user_defined_unit_dose_type_id] INT             NULL,
+    [user_defined_unit_dose_size]    DECIMAL (18, 4) NULL,
+    CONSTRAINT [PK_t_plan_medicine_rule_item] PRIMARY KEY CLUSTERED ([id] ASC)
 );
+
+
 
 
 
