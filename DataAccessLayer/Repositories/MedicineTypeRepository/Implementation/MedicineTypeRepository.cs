017f4bcfb3cc1b120064c8cfa49f72799ee8806c
Radu Mitache <rmitache@hotmail.com>
2019-03-04 20:57:49 +0100

    Changes

--- DataAccessLayer/Repositories/MedicineTypeRepository/Implementation/MedicineTypeRepository.cs
+++ DataAccessLayer/Repositories/MedicineTypeRepository/Implementation/MedicineTypeRepository.cs
@@ -38,35 +38,6 @@ namespace DataAccessLayer.Repositories.MedicineTypeRepository
 
             // Update the TMedicineType
             entitiesContext.TMedicineType.Update(dataEntity);
-
-            // If the IsPackagedIntoUnits field was modified
-            if (dataEntity.IsPackagedIntoUnits != entity.IsPackagedIntoUnits)
-            {
-                // If it was set to true 
-                if (dataEntity.IsPackagedIntoUnits == true)
-                {
-                    // Update all RuleItem records which point to this medType by setting their user_defined_dose fields to NULL 
-
-                }
-                // If it was set to false 
-                else if (dataEntity.IsPackagedIntoUnits == false)
-                {
-                    // OBS: if you set the medType's packaged fields to NULL, what about all RuleItems referring to it ? - they will need a way 
-
-                }
-
-
-
-                // Ideas about setting IsPackagedIntoUnits to false (from true)
-                // A. Disable editing when the MedType has related RuleItems
-                // B. Don't allow changing IsPackagedIntoUnits to false IF the medType has related RuleItems - eg: 
-                // C. Ask the user to choose a UserDefined unitDoseType for the number of RuleItems they have 
-                    // - can get complicated, would it be just for current stuff ? or also for past ? what if they used multiple types ? eg: "i dont remember?"
-                    // - edit: not complicated, because IF it was locked into IsPackagedIntoUnits= true, then ALL RuleItems refer to a SINGLE unitdose
-            }
-
-
-
             entitiesContext.SaveChanges();
             return dataEntity;
         }
